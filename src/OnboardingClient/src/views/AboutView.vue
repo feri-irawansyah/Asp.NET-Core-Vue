9294fbec44308afced3f89f0d6175cd2b6f8b422
Feri Irawansyah <mreza.fauzi@micropiranti.com>
2026-02-04 12:27:47 +0700

    Jajal hasky

--- src/OnboardingClient/src/views/AboutView.vue
+++ src/OnboardingClient/src/views/AboutView.vue
@@ -1,15 +1,32 @@
+<script setup>
+import { ref, watchEffect } from 'vue';
+
+let data = ref([]);
+
+watchEffect(async () => {
+    const res = await fetch('/api/WeatherForecast');
+    data.value = await res.json();
+})
+
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <h1>This is an about page</h1>
+        <ul>
+            <li v-for="item in data">{{ item.summary }}</li>
+        </ul>
+    </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+    .about {
+        min-height: 100vh;
+        display: flex;
+        align-items: start;
+        flex-direction: column;
+        justify-content: center;
+    }
 }
 </style>
