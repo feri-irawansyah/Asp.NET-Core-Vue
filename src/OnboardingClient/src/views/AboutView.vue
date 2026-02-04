f9217f937c1998700eab5be08cf91e1234eda080
Feri Irawansyah <mreza.fauzi@micropiranti.com>
2026-02-04 14:43:04 +0700

    Create linter

--- src/OnboardingClient/src/views/AboutView.vue
+++ src/OnboardingClient/src/views/AboutView.vue
@@ -1,37 +1,15 @@
-<script setup>
-import { ref, watchEffect } from 'vue';
-
-let data = ref([]);
-
-watchEffect(async () => {
-    const res = await fetch('/api/WeatherForecast');
-    data.value = await res.json();
-})
-
-</script>
-
 <template>
-    <div class="about">
-        <h1>This is an about page</h1>
-        <h1>
-            This is an about page
-        </h1>
-        <ul>
-            <li v-for="item in data">
-                {{ item.summary }}
-            </li>
-        </ul>
-    </div>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
-    .about {
-        min-height: 100vh;
-        display: flex;
-        align-items: start;
-        flex-direction: column;
-        justify-content: center;
-    }
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
 }
 </style>
