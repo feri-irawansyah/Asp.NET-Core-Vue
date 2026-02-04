d6c3959fc61682bad09d96317026f42fe369c517
Feri Irawansyah <mreza.fauzi@micropiranti.com>
2026-02-04 12:28:21 +0700

    iiiii

--- src/OnboardingClient.Api/Program.cs
+++ src/OnboardingClient.Api/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer()
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
